72dacd0c5306ff2345e30fb35a26fa40b0e6aa77
jawa0919 <jawa0919@163.com>
2022-03-26 21:42:14 +0800

    add erudaDebug

--- vue_demo/src/main.js
+++ vue_demo/src/main.js
@@ -4,6 +4,13 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+import eruda from "eruda";
+const lsDebug = localStorage.getItem("erudaDebug") == "init";
+const uaDebug = navigator.userAgent.includes("erudaDebug");
+if (lsDebug || uaDebug) {
+  eruda.init();
+}
+
 new Vue({
   router,
   render: (h) => h(App),
