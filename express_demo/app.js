6e27ab182142ef7c4a23f1ea5bef6be4bc906fe3
jawa0919 <jawa0919@163.com>
2022-03-26 16:17:42 +0800

    add static

--- express_demo/app.js
+++ express_demo/app.js
@@ -19,8 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+/// ser vue_demo
+var history = require("connect-history-api-fallback");
+app.use(express.static(path.join(__dirname, "../vue_demo/dist")));
+app.use(history());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- vue_demo/vue.config.js
+++ vue_demo/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const isOfflineMode = process.env.VUE_APP_OFFLINE_OUTPUT_DIR == "distOffline";
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: isOfflineMode ? "./" : "/",
+  outputDir: isOfflineMode ? process.env.VUE_APP_OFFLINE_OUTPUT_DIR : "dist",
 });
